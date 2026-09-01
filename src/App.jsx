0a2b2186322ee60af641154cfd3bdf9c243a878f
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:26:07 -0300

    feat(seo): activate route metadata

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import SeoManager from './components/SeoManager';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/app"
-        element={(
-          <ProtectedRoute>
-            <DashboardPage />
-          </ProtectedRoute>
-        )}
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <SeoManager />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/app"
+          element={(
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          )}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
