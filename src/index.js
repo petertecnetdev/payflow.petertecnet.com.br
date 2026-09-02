9492bb3d715e844d0d88246dfdac712ec117d5dd
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 11:56:45 -0300

    feat: habilita launcher do ecossistema no PayFlow

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ import './styles/company-context.css';
 import './styles/commercial.css';
 import './styles/brand.css';
 import App from './App';
+import PeterAccountGateway from './components/PeterAccountGateway';
 
 document.documentElement.style.setProperty('--payflow-logo', `url(${process.env.PUBLIC_URL}/logo.png)`);
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://api.petertecnet.com.br/api';
+const APP_SLUG = 'payflow';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PeterAccountGateway apiBaseUrl={API_BASE_URL} appSlug={APP_SLUG}>
+        <App />
+      </PeterAccountGateway>
     </BrowserRouter>
   </React.StrictMode>
 );
